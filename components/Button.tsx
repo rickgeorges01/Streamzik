4491fb9725078a2a5dc11c63b7453339371047d6
Rick Georges <94584894+rickgeorges01@users.noreply.github.com>
2024-04-05 00:10:46 +0200

    Finished build the layout of the app

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,8 +1,11 @@
+// Importation de `forwardRef` de React pour la transmission de référence d'un composant parent.
 import {forwardRef} from "react";
 import {twMerge} from "tailwind-merge";
 
+// Extension des attributs standards d'un bouton HTML pour notre interface de props.
 interface  ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
+// Création du composant `Button` qui est enveloppé par `forwardRef` pour accéder à la `ref` de l'élément bouton.
 const Button = forwardRef<HTMLButtonElement,ButtonProps>(({
     className,
     children,
@@ -10,6 +13,8 @@ const Button = forwardRef<HTMLButtonElement,ButtonProps>(({
     type = "button",
     ...props
 }, ref) =>{
+
+    // Rendu de l'élément `button` avec les props et la `ref` appliquées.
     return (
         <button
             type={type}
@@ -36,6 +41,7 @@ const Button = forwardRef<HTMLButtonElement,ButtonProps>(({
         </button>
     )
 })
-
+// Définition du nom d'affichage pour faciliter le débogage.
 Button.displayName = "Button"
+
 export default Button;
