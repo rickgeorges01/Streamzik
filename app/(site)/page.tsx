cd4611f16bd225ba5af07b2f3d17ec8c3e55531c
Rick Georges <94584894+rickgeorges01@users.noreply.github.com>
2024-04-04 22:10:36 +0200

    Start building the Header with more components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,19 @@
 import Image from "next/image";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
-    <div className="text-green-500">
-      Main content, this is so nice!
+    <div className="
+        bg-neutral-900
+        rounded-lg
+        h-full
+        overflow-hidden
+        overflow-y-auto
+    ">
+        <Header>
+            Wait a minute i am working on it !
+        </Header>
+
     </div>
   );
 }
